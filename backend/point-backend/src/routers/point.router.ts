6e37540fac30bd7352769c8bed7cbd55104b9368
sangyun lee <sororiri@gmail.com>
2023-06-22 01:50:31 +0900

    feat: 대회/기록 백엔드 외 파일 삭제 - 개선: 도메인별로 백엔드 릴리즈 브랜치를 별도로 두기 위함 - 변경 사유: 도메인을 모두 병합하여 코드 관리 시 CI / CD 관리가 어려움

--- backend/point-backend/src/routers/point.router.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import pointCtl from '../controllers/point.controller';
-import {asyncWrap} from '../utils/myutils';
-const router = express.Router();
-
-// routing
-router.get('/', asyncWrap(pointCtl.getPoints));
-
-export default router;
