b4b9fa6c52518a61d754715f2b6976b3bb87335f
sangyun lee <sororiri@gmail.com>
2023-06-22 01:16:23 +0900

    feat: record 서버에 DB 연결 테스트하는 API 추가 - DB 연결 테스트를 위한 API 생성

--- backend/race-backend/src/app.ts
+++ backend/race-backend/src/app.ts
@@ -18,8 +18,35 @@ export const createApp = () => {
   app.use(express.json());
   app.use(morgan(morganCustomFormat));
   app.use(router);
+
   app.get('/', (req, res) => {
     res.status(200).send('server is alive');
   })
+  app.post('/connectTest', (req, res) => {
+    res.status(200).send('TEST');
+    connectMYSQL(req.body);
+  })
   return app;
 };
+
+import { DataSource } from 'typeorm';
+
+export const connectMYSQL = (body : any) => {
+  const dataSource = new DataSource({
+    type: body.connect || 'mysql',
+    host: body.host || 'for-tf-race-record.c8xl53x8kbuy.ap-northeast-2.rds.amazonaws.com',
+    port: Number(body.port || 3306),
+    username: body.user || 'admin',
+    password: body.pws || '12345678',
+    database: body.db || 'race',
+  });
+
+  dataSource.initialize()
+    .then(() => {
+      console.log("Data Source has been initialized!");
+    })
+    .catch((e) => {
+      console.log("DB 연결에 실패했습니다. \n.env-example 파일을 올바르게 수정해야합니다.");
+      console.log("error: ", e);
+  })
+}
