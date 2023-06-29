af1fba5c85a4b4423e900c3796206530185d5a62
sangyun lee <sororiri@gmail.com>
2023-06-29 21:42:24 +0900

    feat: 대회기록 백엔드 소스 추가 - 대회 기록 브랜치의 최신 리소스 추가   브랜치: release/ecs-race-record

---
+++ backend/race-backend/src/controllers/official-record.controller.ts
@@ -0,0 +1,61 @@
+import express from 'express';
+import dataSource from "./database";
+
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+const client = new SQSClient({ region: "ap-northeast-2" });
+
+const createRecords = async (req: express.Request, res: express.Response) => {
+  const {user_id, competition_id} = req.body;
+  if (!user_id || !competition_id) {
+    res.status(400).send("대회 ID 혹은 사용자 ID 를 전달받지 못했습니다.")
+    return;
+  }
+  await dataSource.query(
+    `
+      INSERT INTO
+        record
+        (competition_id, user_id, record_time)
+      VALUES
+        (?,?,?)
+    `,
+    [competition_id, user_id, '1998-01-23 12:45:56']
+  );
+
+  res.status(200).send("점수가 추가되었습니다.")
+  sendMessage(String(user_id));
+}
+
+const getRecords = async (req: express.Request, res: express.Response) => {
+  const records = await dataSource.query(
+    `
+      SELECT
+        competition_id,
+        user_id,
+        record_time
+      FROM
+        record
+    `,
+  );
+  res.status(200).json(records)
+}
+
+export default {
+  createRecords,
+  getRecords,
+}
+
+const sendMessage = async (userid: string) => {
+  // Create an SQS service object
+  var params = {
+    DelaySeconds: 10,
+    MessageAttributes: {
+      "UserId": {
+        DataType: "String",
+        StringValue: userid
+      }
+    },
+    MessageBody: "please insert point by userId",
+    QueueUrl: process.env.QUEUE_URL
+  };
+  await client.send(new SendMessageCommand(params));
+}
